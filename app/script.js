4f4d4e9808922bb56f9b8900988abd6e30eeb679
eltonrs25 <eltonrs25>
2021-11-27 21:35:33 +0500

    Added client side script to fetch books and display html table

--- app/script.js
+++ app/script.js
@@ -3,5 +3,19 @@ const API_URL = `http://localhost:8000/api/books`;
 function getList() {
     fetch(API_URL)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        let html = ``;
+        data.forEach(book => {
+            html += `<tr>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>$${book.price}</td>
+                <td>${book.rating}</td>
+                <td>${book.genre}</td>
+                <td>${book.year}</td>
+            </tr>`
+        });
+
+        document.getElementById('tbl-data').innerHTML = html;
+    });
 }
