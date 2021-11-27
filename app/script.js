e433d1ab5192f9ad3f8eb41f66f29a0f333ccfc0
eltonrs25 <eltonrs25>
2021-11-28 01:08:13 +0500

    Added some valuable comments in the app section

--- app/script.js
+++ app/script.js
@@ -1,10 +1,17 @@
+
+// API URL
 const API_URL = `http://localhost:8000/api/books`;
 
+// this function will be used to fetch all books from API
 function getList() {
+    // using the fetch to get data
+    // converting it to JSON and then using the JSON data
     fetch(API_URL)
     .then(response => response.json())
     .then(data => {
+        // create empty variable for table html
         let html = ``;
+        // loop through all books and generate its html
         data.forEach(book => {
             html += `<tr>
                 <td>${book.title}</td>
@@ -26,11 +33,14 @@ function getList() {
             </tr>`
         });
 
+        // set the html on UI
         document.getElementById('tbl-data').innerHTML = html;
     });
 }
 
+// this method will be used to create new book
 function add() {
+    // getting data from input controls
     let title = document.getElementById('txt-title').value;
     let author = document.getElementById('txt-author').value;
     let price = document.getElementById('txt-price').value;
@@ -38,21 +48,25 @@ function add() {
     let genre = document.getElementById('txt-genre').value;
     let year = document.getElementById('txt-year').value;
 
+    // validations
     if(!title || !author || !price || !rating || !genre || !year) {
         alert(`All fields are required.`);
         return;
     }
 
+    // validations
     if(parseInt(rating) < 0 || parseInt(rating) > 10) {
         alert(`Rating should be between 0 and 10.`);
         return;
     }
 
+    // validations
     if(year.length !== 4) {
         alert(`Please provide a valid year.`);
         return;
     }
 
+    // create object for all data
     let data = {
         title,
         author,
@@ -62,39 +76,49 @@ function add() {
         year
     };
 
-    
+    // check if the ID is in URL
+    // if ID exists, it means we are updating existing record,
+    // otherwise, we are creating new record
     let id = window.location.href.split('id=').pop();
+
+    // ID check
     if(!id || id.length !== 24) {
+        // sending API request to create new book
         fetch(API_URL, {
             method: "POST",
-            body: JSON.stringify(data),
+            body: JSON.stringify(data), // sending book data as JSON
             headers: { 'Content-Type': 'application/json' }
         })
         .then(response => response.json())
         .then(data => {
+            // alert success and redirect to list page
             alert("Book saved successfully!");
             window.location.href = "index.html";
         });
     }
     else {
+        // sending API request to update an existing book
         fetch(`${API_URL}/${id}`, {
             method: "PUT",
-            body: JSON.stringify(data),
+            body: JSON.stringify(data), // sending book data as JSON
             headers: { 'Content-Type': 'application/json' }
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            // alert success and redirect to list page
             alert("Book saved successfully!");
             window.location.href = "index.html";
         });
     }
 }
 
+// this method will be called when user clicks on Edit button
 function edit(id) {
     window.location.href = `new.html?id=${id}`;
 }
 
+// this method will be used to fetch a record from DB and display it on the scren
+// it will be used for updating a record
 function load() {
     let id = window.location.href.split('id=').pop();
     
@@ -112,6 +136,7 @@ function load() {
     });
 }
 
+// this method will be used for deletion purpose
 function deleteBook(id) {
     if(confirm(`Are you sure you want to delete it?`)) {
         fetch(`${API_URL}/${id}`, {
