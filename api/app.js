88bb31207a3d2bcef8d25a3864b60dba97781c7b
eltonrs25 <eltonrs25>
2021-11-28 01:02:22 +0500

    Added some valuable comments in API

--- api/app.js
+++ api/app.js
@@ -2,13 +2,12 @@ var express = require('express');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var app = express();
-var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8000;
 app.use(cors());
-app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
 
 const CONNECTION_STRING = `mongodb://localhost:27017/books`;
 mongoose.connect(CONNECTION_STRING);
@@ -18,7 +17,7 @@ var Book = require('./models/book');
 
 //get all book data from db
 app.get('/api/books', function (req, res) {
-    // use mongoose to get all todos in the database
+    // use mongoose to get all books in the database
     Book.find(function (err, books) {
         // if there is an error retrieving, send the error otherwise send data
         if (err)
@@ -29,11 +28,14 @@ app.get('/api/books', function (req, res) {
 
 // get a book with ID of 1
 app.get('/api/books/:book_id', function (req, res) {
+    // get the ID parameter from params
     let id = req.params.book_id;
+    // use model to find a book by its ID
     Book.findById(id, function (err, book) {
         if (err)
             res.send(err)
 
+        // return the found book to app
         res.json(book);
     });
 
@@ -43,13 +45,12 @@ app.get('/api/books/:book_id', function (req, res) {
 // create book and send back all books after creation
 app.post('/api/books', function (req, res) {
 
-    console.log(req.body);
-
     // create mongose method to create a new record into collection
     Book.create(req.body, function (err, book) {
         if (err)
             res.send(err);
 
+        // return the created book to app
         res.json(book);
     });
 
@@ -62,10 +63,11 @@ app.put('/api/books/:book_id', function (req, res) {
     // create mongose method to update an existing record into collection
     let id = req.params.book_id;
 
-    // save the user
+    // update the book data
     Book.findByIdAndUpdate(id, req.body, function (err, book) {
         if (err) throw err;
 
+        // return the book to app
         res.json(book);
     });
 });
@@ -73,14 +75,18 @@ app.put('/api/books/:book_id', function (req, res) {
 // delete a book by id
 app.delete('/api/books/:book_id', function (req, res) {
 
+    // get the ID parameter from params
     let id = req.params.book_id;
+    // find the book by ID and delete it
     Book.findByIdAndDelete(id, function (err) {
         if (err)
             res.send(err);
         
+        // return response to app
         res.json({status: true});
     });
 });
 
+// start the ExpressJS server
 app.listen(port);
 console.log("App listening on port : " + port);
